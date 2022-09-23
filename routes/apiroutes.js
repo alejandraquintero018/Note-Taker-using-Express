47a9b5a9d69aae451eff2669eb8361613e5a2823
alejandraquintero018 <alejandraquintero018@gmail.com>
2022-09-23 08:19:50 -0700

    debugged the project since I was getting a MIME error in the browser, I also worked further on the routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,28 +1,33 @@
 const router = require('express').Router(); 
 const fs = require('fs'); 
+const notes = require('../db/db.json'); 
 
-
+//recieving the notes 
 router.get('/notes', (req, res) => {
-    const notes = require('/db/db.json'); 
-//sending back the notes 
     res.status(200).json(notes); 
 })
 
+//sending back the notes 
 router.post('/notes', (req, res) => {
-    const notes = ('/db/db.json'); 
+    const notes = ('../db/db.json'); 
 
-    const { noteTitle, noteText } = req.body; 
-    console.log(noteText, noteTitle); 
+    const { title, text } = req.body; 
+    console.log(text, title); 
 
-   fs.writeFile('/db/db.json', JSON.stringify(notes), (err) => {
+    fs.readFile('../db/db.json', (err, data)=> {
+        if (err) {
+            console.log(err); 
+        }
+        notes.push(req.body); 
+   
+   fs.writeFile('../db/db.json', JSON.stringify(notes), (err) => {
     if(err) {
         res.status(500).end(); 
     }else {
         res.status(200).end(); 
     }
    });
-
-   res.end(); 
+}); 
 })
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express'); 
 const fs = require('fs'); 
 const apiroutes = require('./routes/apiroutes.js'); 
+const htmlRoutes = require('./routes/htmlRoutes.js'); 
 const path = require('path');
 
 const app = express(); 
 
-
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
-
+//this line will allow me to use all of the files in the public folder 
+app.use(express.static('public'));
 app.use('/api', apiroutes); 
-//this line will allow me to all of the files in the public folder 
-app.use(express.static('public')); 
+app.use("/", htmlRoutes); 
 
-//GET route for homepage 
-app.get('/', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html')));
-
-//GET route for Notes page
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'notes.html')));
-
+// app.get(apiroutes, (req, res) =>{
+//     res.status(200).json(`${req.method} request recieved `); 
+// })
 
 app.listen(3002, () => console.log('App running on http://localhost:3002/'))
