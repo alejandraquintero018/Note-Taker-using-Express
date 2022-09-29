784513e893a4eb0064ce23b5c197e42128ae4203
alejandraquintero018 <alejandraquintero018@gmail.com>
2022-09-29 11:18:28 -0700

    finished the routes and attempted to do the delete route. The app has full functionality

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,33 +1,54 @@
-const router = require('express').Router(); 
-const fs = require('fs'); 
-const notes = require('../db/db.json'); 
+const router = require('express').Router();
+const fs = require('fs');
+const notes = require('../db/db.json');
+const path = require('path');
 
 //recieving the notes 
 router.get('/notes', (req, res) => {
-    res.status(200).json(notes); 
-})
+    res.json(notes);
+});
 
 //sending back the notes 
 router.post('/notes', (req, res) => {
-    const notes = ('../db/db.json'); 
 
-    const { title, text } = req.body; 
-    console.log(text, title); 
-
-    fs.readFile('../db/db.json', (err, data)=> {
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
         if (err) {
-            console.log(err); 
+            res.status(500).end();
         }
-        notes.push(req.body); 
-   
-   fs.writeFile('../db/db.json', JSON.stringify(notes), (err) => {
-    if(err) {
-        res.status(500).end(); 
-    }else {
-        res.status(200).end(); 
-    }
-   });
-}); 
+        notes.push(req.body);
+    
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) {
+                res.status(500).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    }); 
 })
 
+// router.delete('/notes/:id', (req, res)=> {
+//     res.send(req.params); 
+//     const noteId = req.params.id; 
+
+//     fs.readFile(path.join(__dirname, '../db/db.json'), (err, notes) => {
+//         if (err) {
+//             res.status(500).end();
+//         }
+//         notes.forEach(note => {
+//             if (noteId == note.id){
+              
+//             }
+//         });
+    
+//         fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+//             if (err) {
+//                 res.status(500).end();
+//             } else {
+//                 res.status(200).end();
+//             }
+//         });
+//     }); 
+
+// })
 module.exports = router; 
